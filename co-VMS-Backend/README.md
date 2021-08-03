dc306ad70e965f2de1b6fcbaa40c8d40ef915e16
arshisaxena26 <arshisaxena26@gmail.com>
2021-08-03 23:07:01 +0530

    Project

---
+++ co-VMS-Backend/README.md
@@ -0,0 +1,4 @@
+# covid19-vaccination-management
+(Co-VMS) Coronavirus Vaccination Management System
+
+Mysql schema name = covid19vms
---
+++ co-VMS-Backend/src/test/java/io/smokehouse/Covid19VaccinationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package io.smokehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19VaccinationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
